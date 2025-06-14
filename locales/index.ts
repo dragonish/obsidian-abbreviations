a0b378bf9306c1e7265e1b815e21ce20dc4f5bb1
dragonish <no.web.developer@outlook.com>
2025-06-14 18:00:58 +0800

    ui(locales): add Traditional Chinese language

--- locales/index.ts
+++ locales/index.ts
@@ -1,8 +1,10 @@
 import * as en from "./en.json";
 import * as zh from "./zh.json";
+import * as zhTW from "./zh-TW.json";
 import { ObsidianPluginI18n } from "obsidian-plugin-i18n";
 
 export const i18n = new ObsidianPluginI18n({
   en,
   zh,
+  "zh-TW": zhTW,
 });
